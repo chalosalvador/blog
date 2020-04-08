4a654310d6a9db638ea71b19b1b147534fcd2f46
chalosalvador <chalosalvador@gmail.com>
2020-04-08 10:20:46 -0500

    #4 #5 Seeding tables

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Article;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Vaciar la tabla.
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Crear artículos ficticios en la tabla
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Vaciar la tabla
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Crear la misma clave para todos los usuarios
+        // conviene hacerlo antes del for para que el seeder
+        // no se vuelva lento.
+        $password = Hash::make('123456');
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@prueba.com',
+            'password' => $password,
+        ]);
+
+        // Generar algunos usuarios para nuestra aplicacion
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
