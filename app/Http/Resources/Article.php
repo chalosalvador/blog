aef689ee48574a7627fe17211002217ffc1e6bf0
chalosalvador <chalosalvador@gmail.com>
2020-05-06 17:39:51 -0500

    #14 Resources (Articles)

---
+++ app/Http/Resources/Article.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Category;
+use App\User;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Article extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'created_at' => $this->created_at,
+            'user' => "/api/users/" . $this->user_id,
+            'category' => "/api/categories/" . $this->category_id,
+        ];
+    }
+}
