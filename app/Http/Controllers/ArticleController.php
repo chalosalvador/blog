8d5dc91898a10f5afa976ca9b651c197543b0089
chalosalvador <chalosalvador@gmail.com>
2020-05-11 08:43:23 -0500

    #17 ArticlePolicy

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,11 +17,13 @@ class ArticleController extends Controller
 
     public function index()
     {
+//        $this->authorize('viewAny', Article::class);
         return new ArticleCollection(Article::paginate(10));
     }
 
     public function show(Article $article)
     {
+        $this->authorize('view', $article);
         return response()->json(new ArticleResource($article), 200);
     }
 
@@ -31,6 +33,8 @@ class ArticleController extends Controller
 
     public function store(Request $request)
     {
+        $this->authorize('create', Article::class);
+
         $request->validate([
             'title' => 'required|string|unique:articles|max:255',
             'body' => 'required',
@@ -52,6 +56,8 @@ class ArticleController extends Controller
 
     public function update(Request $request, Article $article)
     {
+        $this->authorize('update', $article);
+
         $request->validate([
             'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
             'body' => 'required',
@@ -64,6 +70,8 @@ class ArticleController extends Controller
 
     public function delete(Request $request, Article $article)
     {
+        $this->authorize('delete', $article);
+
         $article->delete();
         return response()->json(null, 204);
     }
