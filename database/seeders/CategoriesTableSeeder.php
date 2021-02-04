6256391fe9cd975d2b6ed7e0dfe00d76f273990d
chalosalvador <chalosalvador@gmail.com>
2021-02-04 16:15:11 -0500

    Faker

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Category;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder
@@ -16,7 +17,7 @@ class CategoriesTableSeeder extends Seeder
     {
         // Vaciamos la tabla categories
         Category::truncate();
-        $faker = \Faker\Factory::create();
+        $faker = Factory::create();
 
         for ($i = 0; $i < 3; $i++) {
             Category::create([
