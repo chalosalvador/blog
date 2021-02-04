22eaf0000f5d62cebf8df74531a72d3af1fdedcb
chalosalvador <chalosalvador@gmail.com>
2021-02-04 17:42:33 -0500

    logout without cookie

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -114,7 +114,7 @@ class UserController extends Controller
             return response()->json([
                 "status" => "success",
                 "message" => "User successfully logged out."
-            ], 200)->withoutCookie($cookie);
+            ], 200)->withCookie($cookie);
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
             return response()->json(["message" => "No se pudo cerrar la sesión."], 500);
